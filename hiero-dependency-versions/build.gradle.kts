706ef4a3cc07298c71c17760584a1cce0cbf283d
Jasper Potts <1466205+jasperpotts@users.noreply.github.com>
2025-04-30 14:38:08 -0700

    Removed besu copy of v24.3.3

--- hiero-dependency-versions/build.gradle.kts
+++ hiero-dependency-versions/build.gradle.kts
@@ -107,8 +107,8 @@ dependencies.constraints {
     api("org.eclipse.collections:eclipse-collections:$eclipseCollections") {
         because("org.eclipse.collections.impl")
     }
-    api("org.hyperledger.besu:besu-datatypes:$besu") { because("org.hyperledger.besu.datatypes") }
-    api("org.hyperledger.besu:evm:$besu") { because("org.hyperledger.besu.evm") }
+//    api("org.hyperledger.besu:besu-datatypes:$besu") { because("org.hyperledger.besu.datatypes") }
+//    api("org.hyperledger.besu:evm:$besu") { because("org.hyperledger.besu.evm") }
     api("org.hyperledger.besu:secp256k1:0.8.2") {
         because("org.hyperledger.besu.nativelib.secp256k1")
     }
